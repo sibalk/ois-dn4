d2bc1392f4c8ab08b866d356cdccdde2d014848d
Klemen Šibal <klemen.sibal@gmail.com>
2014-12-08 16:52:23 +0100

    Dodana uvodna stran, dodan tudi polje za spremembo

--- animacija.js
+++ animacija.js
@@ -1,23 +1,47 @@
-var isProfileOpen=true;
+var isProfileAddOpen=false;
+var isProfileEditOpen=false;
 function closeProfileAdd () {
 	$("#profileAdd").animate({
 		height: 0,
 		borderWidth: 0
 	}, 1000, function(){
-		isProfileOpen=false
+		isProfileAddOpen=false
 	})
 }
 function openProfileAdd () {
 	$("#profileAdd").animate({
 		height: $("#profileAdd").get(0).scrollHeight
 	}, 1000, function(){
-		isProfileOpen=true
+		isProfileAddOpen=true
 	})
 }
 function ProfileAdd(){
-	if (isProfileOpen) {
+	if (isProfileAddOpen) {
 		closeProfileAdd()
 	}
 	else
 		openProfileAdd()
+}
+function closeProfileEdit () {
+	$("#profileEdit").animate({
+		height: 0,
+		borderWidth: 0
+	}, 1000, function(){
+		isProfileEditOpen=false
+	})
+}
+function openProfileEdit () {
+	$("#profileEdit").animate({
+		height: $("#profileEdit").get(0).scrollHeight
+	}, 1000, function(){
+		isProfileEditOpen=true
+	})
+}
+function ProfileEdit() {
+	if (isProfileEditOpen) {
+		closeProfileEdit();
+	}
+	else{
+		openProfileEdit();
+	}
 }
